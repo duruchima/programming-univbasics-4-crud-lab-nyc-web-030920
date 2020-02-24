1e564e33860a7874857fb92965eed67b915880c9
Duncan Uruchima <duruchima1@gmail.com>
2020-02-24 19:03:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["john", "bob"]
-  array.push["bill"]
+  array.push("bill")
 end
 
 def add_element_to_start_of_array(array, element)
